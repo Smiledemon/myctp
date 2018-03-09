a926173f5cf4f8f839aa0045e4270dca0c52b420
Smiledemon <790333234@qq.com>
2018-03-09 16:16:50 +0800

    test

--- src/main/java/com/xceder/demo/MdTest.java
+++ src/main/java/com/xceder/demo/MdTest.java
@@ -2,6 +2,7 @@ package com.xceder.demo;
 
 import com.xceder.ctp.market.CThostFtdcMdApi;
 import com.xceder.ctp.market.CThostFtdcMdSpi;
+import org.bridj.BridJ;
 import org.bridj.Pointer;
 
 /**
@@ -14,6 +15,8 @@ import org.bridj.Pointer;
  */
 public class MdTest {
     public static void main(String[] args) {
+        BridJ.register(CThostFtdcMdApi.class);
+        BridJ.register(CThostFtdcMdSpi.class);
         CThostFtdcMdApi mdApi = CThostFtdcMdApi.CreateFtdcMdApi(Pointer.pointerToCString("./"),false,false).get();
 
         CThostFtdcMdSpi mdSpi = new MdSpi();
